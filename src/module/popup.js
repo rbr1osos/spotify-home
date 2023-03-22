721337f97ddfb3e3139d8de6428365f62f4426bc
Ryan Briosos <rybri415@yahoo.com>
2023-03-22 05:41:00 -0700

    finished

--- src/module/popup.js
+++ src/module/popup.js
@@ -30,18 +30,35 @@ const form = document.createElement('FORM')
 form.setAttribute('id','create-form')
 
 const username_container = document.createElement('div')
+username_container.setAttribute('id','username-container')
 const username_name = document.createElement('h2')
 username_name.innerHTML='Username';
 const username_input = document.createElement('INPUT')
 username_input.setAttribute('id','username')
 username_input.setAttribute('type','text')
 username_input.setAttribute('name','username')
+username_input.setAttribute('minlength','4')
+username_input.setAttribute('maxlength','20')
+username_input.setAttribute("pattern","^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
 username_input.setAttribute('placeholder','Enter username')
+username_input.required;
+username_input.addEventListener('input', (e)=>{
+    if (username_input.validity.valid){
+        username_input.style.border=''
+        username_input.style.backgroundColor=''
+        username_container.classList.remove('active')
+    }
+    else{
+        showError(username_input.id);
+    }
+})
 username_container.appendChild(username_name)
 username_container.appendChild(username_input)
+
 form.appendChild(username_container)
 
 const email_container = document.createElement('div')
+email_container.setAttribute('id','email-container')
 const email_name =  document.createElement('h2')
 email_name.innerHTML='Email'
 const email_input = document.createElement('INPUT')
@@ -49,6 +66,17 @@ email_input.setAttribute('id','email')
 email_input.setAttribute('type','email')
 email_input.setAttribute('name','email')
 email_input.setAttribute('placeholder','email')
+email_input.addEventListener('input', (e)=>{
+    if (email_input.validity.valid){
+        email_input.style.border=''
+        email_input.style.backgroundColor=''
+        email_container.classList.remove('active')
+
+    }
+    else{
+        showError(email_input.id);
+    }
+})
 email_container.appendChild(email_name)
 email_container.appendChild(email_input)
 form.appendChild(email_container)
@@ -68,13 +96,27 @@ country_container.appendChild(country_input)
 form.appendChild(country_container)
 
 const zip_container = document.createElement('div')
+zip_container.setAttribute('id','zip-container')
 const zip_name = document.createElement('h2')
 zip_name.innerHTML='Zipcode'
 const zip_input = document.createElement('INPUT')
 zip_input.setAttribute('id','zip')
 zip_input.setAttribute('type','text')
 zip_input.setAttribute('name','zip')
-zip_input.setAttribute('placeholder','99999') //need validation
+zip_input.setAttribute('placeholder','99999') 
+zip_input.setAttribute('maxlength','5')
+zip_input.setAttribute('pattern','^[0-9]*$');
+zip_input.addEventListener('input', (e)=>{
+    if (zip_input.validity.valid){
+        zip_input.style.border=''
+        zip_input.style.backgroundColor=''
+        zip_container.classList.remove('active')
+
+    }
+    else{
+        showError(zip_input.id);
+    }
+})
 zip_container.appendChild(zip_name)
 zip_container.appendChild(zip_input)
 form.appendChild(zip_container)
@@ -86,21 +128,45 @@ const password_input = document.createElement('INPUT')
 password_input.setAttribute('id','password')
 password_input.setAttribute('type','password')
 password_input.setAttribute('name','password')
-password_input.setAttribute('placeholder','enter password')
+password_input.setAttribute('placeholder','enter password') //must contain at least 8 char, 1 num, and include lower and upper case, and special char
+password_input.addEventListener('input', (e)=>{
+    if (password_input.validity.valid){
+        console.log('nice')
+        password_input.style.border=''
+        password_input.style.backgroundColor=''
+    }
+    else{
+        showError(password_input.id);
+    }
+})
 password_container.appendChild(password_name)
 password_container.appendChild(password_input)
 form.appendChild(password_container)
 
 const confirm_container = document.createElement('div')
+confirm_container.setAttribute('id','confirm-container')
 const confirm_name = document.createElement('h2')
 confirm_name.innerHTML='Confirm Password'
 const confirm_input = document.createElement('INPUT')
 confirm_input.setAttribute('id','confirm')
 confirm_input.setAttribute('type','password')
 confirm_input.setAttribute('name','confirm')
-confirm_input.setAttribute('placeholder','re-enter password')
+confirm_input.setAttribute('placeholder','re-enter password') 
+confirm_input.addEventListener('input', (e)=>{
+    if (confirm_input.value === password_input.value){
+        confirm_input.style.border=''
+        confirm_input.style.backgroundColor=''
+        confirm_container.classList.remove('active')
+    }
+    else{
+        showError(confirm_input.id);
+
+
+    }
+})
 confirm_container.appendChild(confirm_name)
 confirm_container.appendChild(confirm_input)
+
 form.appendChild(confirm_container)
 
 const popup_button = document.createElement('button')
@@ -116,4 +182,42 @@ popup_button.addEventListener('click',()=>{
 popup.appendChild(form)
 popup.appendChild(popup_button)
 
-/* Create form */
+/* form validation */
+function showError(input){
+    if (input === 'username'){
+        if (username_input.validity.tooShort){
+            console.log('Too Short need at least 8 characters')
+            username_container.classList.add('active')
+        }
+
+    }
+    else if (input === 'email'){
+
+        if (email_input.validity.typeMismatch){
+            email_input.style.border='3px solid red'
+            email_input.style.backgroundColor='#FDD'
+            email_container.classList.add('active')
+        }
+        else if (email_input.validity.tooShort){
+            console.log('Too Short')
+            email_input.style.border='3px solid red'
+            email_input.style.backgroundColor='#FDD'
+        }
+    }
+    else if (input === 'zip'){
+        zip_input.style.border='3px solid red'
+        zip_input.style.backgroundColor='#FDD'
+        zip_container.classList.add('active')
+
+    }
+    else if (input === 'password'){
+        console.log('need /must contain at least 8 char, 1 num, and include lower and upper case, and special char')
+        password_input.style.border='3px solid red'
+        password_input.style.backgroundColor='#FDD'
+    }
+    else if (input === 'confirm'){
+        confirm_container.classList.add('active')
+    }
+}
+
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
